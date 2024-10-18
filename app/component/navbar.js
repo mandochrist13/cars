91c96868a7af1f791ec4c69548ace05f1c08e83d
carl07le message <mikolodarselcarl@gmail.com>
2024-10-18 20:29:25 +0100

    commit

--- app/component/navbar.js
+++ app/component/navbar.js
@@ -3,6 +3,9 @@ import Image from "next/image";
 export default function Navbar() {
   return (
     <div className="">
+      <nav className="bg-gray-800 text-white py-4">
+      
+      </nav>
    hello
     </div>
   );
