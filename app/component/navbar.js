e4a80cde1c8685b0da43c3976ec27e1ded781353
carl07le message <mikolodarselcarl@gmail.com>
2024-10-18 21:18:31 +0100

    premier

--- app/component/navbar.js
+++ app/component/navbar.js
@@ -2,11 +2,16 @@ import Image from "next/image";
 
 export default function Navbar() {
   return (
-    <div className="">
-      <nav className="bg-gray-800 text-white py-4">
-      
+    <div className="bg-zinc-700 w-full">
+      <nav className="flex justify-center items-center ">
+        <Image 
+        src="/Ellipse 1.png"
+        width={100}
+        height={100}
+        alt="image"
+        />
+        <h3 className="text-white ">Bonjour, Super Admin</h3>
       </nav>
-   hello
     </div>
   );
-}
+};
